ec8f0ae54a86f2729e87cde6476e7518bfcc0baf
Wonguk Jeong <huchijwk@gmail.com>
2019-01-07 01:36:43 +0900

    [travis ci] coverall integrated
    - gcov option added
    
    Signed-off-by: Wonguk Jeong <huchijwk@gmail.com>

---
+++ scripts/build/gcov.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR=`dirname $0`
+BASEDIR=$SCRIPTDIR/../..
+BUILDDIR=$BASEDIR/build
+
+mkdir $BUILDDIR
+pushd $BUILDDIR
+
+cmake -DENABLE_GCOV=1 ..
+make -j$((`nproc`+1))
+popd
