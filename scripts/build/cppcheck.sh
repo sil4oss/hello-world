743d6d2b3da6b45afcab2187518d542e342eea4f
Wonguk Jeong <huchijwk@gmail.com>
2019-01-06 14:48:39 +0900

    cppcheck build added into travis script
    Signed-off-by: Wonguk Jeong <huchijwk@gmail.com>

---
+++ scripts/build/cppcheck.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR=`dirname $0`
+BASEDIR=$SCRIPTDIR/../..
+BUILDDIR=$BASEDIR/build
+
+mkdir $BUILDDIR
+pushd $BUILDDIR
+
+cmake ..
+make -j$((`nproc`+1)) cppcheck
+popd
